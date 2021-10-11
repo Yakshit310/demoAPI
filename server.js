bdf52c2d9e35c7b366e0cf71595f33b3559a8e73
Yakshit Pant <yakshitpant3102001@gmail.com>
2021-10-11 14:19:35 +0530

    Added basic DemoAPI

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({
+    extended: true
+}))
+app.set('view engine', 'ejs');
+// create a GET route
+app.get("/",(req,res)=>{
+    res.render("index")
+})
+app.post("/",(req,res)=>{
+    res.render("res",{apiKey:req.body.apiKey})
+})
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
